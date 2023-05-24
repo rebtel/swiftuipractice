bb3cd1a92e41245342ad8b260963226f7532ad72
Richmond Ko <richmond.ko@rebtel.com>
2023-05-24 14:22:41 +0200

    Add a profile summary view and a profile button that shows the profile summary

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Richmond Ko on 2023-05-24.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
