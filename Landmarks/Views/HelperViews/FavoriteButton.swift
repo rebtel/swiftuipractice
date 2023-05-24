1f50bf08e79fcc75181b826835852cba1395ac0c
Richmond Ko <richmond.ko@rebtel.com>
2023-05-24 11:27:57 +0200

    Add favorite button and binding of isfavorite

---
+++ Landmarks/Views/HelperViews/FavoriteButton.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Richmond Ko on 2023-05-24.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        FavoriteButton(isSet: .constant(true))
+    }
+}
