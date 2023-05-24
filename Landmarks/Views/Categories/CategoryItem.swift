633ce0606c89799fd5e767ceedfa06b566e975ad
Richmond Ko <richmond.ko@rebtel.com>
2023-05-24 14:09:04 +0200

    Add tabbed main view

--- Landmarks/Views/Categories/CategoryItem.swift
+++ Landmarks/Views/Categories/CategoryItem.swift
@@ -1,23 +1,33 @@
+//
+//  CategoryView.swift
+//  Landmarks
+//
+//  Created by Richmond Ko on 2023-05-24.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
 import SwiftUI
 
-struct CategoryItem: View {
+struct CategoryView: View {
     var landmark: Landmark
-
+    
     var body: some View {
         VStack(alignment: .leading) {
             landmark.image
+                .renderingMode(.original)
                 .resizable()
                 .frame(width: 155, height: 155)
                 .cornerRadius(5)
             Text(landmark.name)
+                .foregroundColor(.primary)
                 .font(.caption)
         }
         .padding(.leading, 15)
     }
 }
 
-struct CategoryItem_Previews: PreviewProvider {
+struct CategoryView_Previews: PreviewProvider {
     static var previews: some View {
-        CategoryItem(landmark: ModelData().landmarks[0])
+        CategoryView(landmark: ModelData().landmarks[0])
     }
 }
--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -8,9 +8,26 @@ A view showing the details for a landmark.
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+        
     var body: some View {
-        //Text("Alper")
-        LandmarkList()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
     }
 }
 
