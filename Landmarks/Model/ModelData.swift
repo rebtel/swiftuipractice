06fa27fccb96eb0eb72952e2569fe9cf6d0f39c5
Richmond Ko <richmond.ko@rebtel.com>
2023-05-24 14:27:36 +0200

    Add edit mode and model data binding to profile host

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -3,6 +3,7 @@ import Foundation
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    @Published var profile = Profile.default
     
     var categories: [String: [Landmark]] {
         Dictionary(
