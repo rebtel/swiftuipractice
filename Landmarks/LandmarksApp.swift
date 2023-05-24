06c7f7dddeb623fb48d85124e0c31f3e8be02fad
Richmond Ko <richmond.ko@rebtel.com>
2023-05-24 10:09:12 +0200

    Initial swift UI app

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Richmond Ko on 2023-05-24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
